1c5536e64ea06ed7739e0ac7ff7af9d1efede65c
Eduardo Araujo <eduardo1996araujo@gmail.com>
2023-11-30 20:37:17 -0300

    first commit

---
+++ src/main/java/com/eduardo/Design/Pattern/Strategy/DesignPatternStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.Design.Pattern.Strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/Design/Pattern/Strategy/DesignPatternStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.Design.Pattern.Strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
